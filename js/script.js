0adcec0388eda51ed49527417e4c224c2cbcaa7b
Matheo Villacres <mathius85203@gmail.com>
2024-10-28 11:52:24 -0500

    About me Info Added
    And buttons for hire me and Download CV

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-alert("This website is under construction! Thanks for trying the demo!");
+// document.addEventListener("DOMContentLoaded", function() {
+
+//     alert("This website is under construction! Thanks for trying the demo!");
+//     const hireMeButton = document.getElementById("hireMeButton"); // Getting ID of the button element
+
+//     hireMeButton.addEventListener("click", function(event) {
+//         event.preventDefault(); // Temporary until Contact section is added
+//         alert("Contact me at: villacrm@umanitoba.ca");
+//     });
+// });
