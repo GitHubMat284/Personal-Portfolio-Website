e2f9b37a546729fecdca71d41e4b4ccb068b5fae
Matheo Villacres <mathius85203@gmail.com>
2024-11-14 14:46:40 -0600

    Display Version

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+alert("This website is under construction! Thanks for trying the demo!");
 // document.addEventListener("DOMContentLoaded", function() {
 
-//     alert("This website is under construction! Thanks for trying the demo!");
 //     const hireMeButton = document.getElementById("hireMeButton"); // Getting ID of the button element
 
 //     hireMeButton.addEventListener("click", function(event) {
